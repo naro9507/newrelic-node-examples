00e0b2218f7f676b24065283009339f3594254c2
Naohiro Ishikawa <ishikawa-naohiro@dmm.com>
2025-08-15 00:35:57 +0900

    feat: 🎸 standalone

--- nextjs/nextjs-app-router/next.config.js
+++ nextjs/nextjs-app-router/next.config.js
@@ -3,6 +3,7 @@
 const nrExternals = require('newrelic/load-externals')
 
 module.exports = {
+  output: 'standalone',
   // For use with Next 14 and earlier:
   experimental: {
     // Without this setting, the Next.js compilation step will routinely
