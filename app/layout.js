6acf96ca6cba12f86b7f6a7d0d25a16ba78de752
Naohiro Ishikawa <ishikawa-naohiro@dmm.com>
2025-08-15 01:16:02 +0900

    feat: 🎸 show connect

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,7 @@ export default async function RootLayout({ children }) {
     hasToRemoveScriptWrapper: true,
     allowTransactionlessInjection: true,
   })
+  const connected = newrelic.agent.collector.isConnected()
 
   return (
     <html>
@@ -46,6 +47,7 @@ export default async function RootLayout({ children }) {
       // object literal.
       dangerouslySetInnerHTML={{__html: browserTimingHeader}}
     />
+    <p>newrelic connected: {String(connected)}</p>
     </body>
     </html>
   )
